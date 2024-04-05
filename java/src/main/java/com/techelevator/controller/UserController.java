2720f14eb1cb0c221f954b095af0026d166f0ecf
Roberto Cedeno <e@robced.com>
2024-04-05 14:21:05 +0000

    Add email value to SQL query in data.sql

---
+++ java/src/main/java/com/techelevator/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.JdbcUserDao;
+import com.techelevator.dao.UserDao;
+import com.techelevator.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+    UserDao userDao;
+
+    public UserController(UserDao userDao){
+        this.userDao = userDao;
+    }
+
+
+    @RequestMapping(path = "/all",method = RequestMethod.GET)
+    public List<User> getUsers(){
+        return userDao.getUsers();
+    }
+
+    @RequestMapping(path = "/{id}",method = RequestMethod.GET)
+    public User getUserById(@PathVariable int id){
+        return  userDao.getUserById(id);
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public User getUserByEmail(@RequestParam("email") String email){
+        return  userDao.getUserByEmail(email);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public User updateUser(@RequestBody User updatedUser){
+        System.out.println("User's password: " + updatedUser.getPassword());
+        return  userDao.updateUser(updatedUser);
+    }
+
+    @RequestMapping(path="/{id}", method= RequestMethod.DELETE)
+    public void deleteUserById(@PathVariable int id){
+        userDao.deleteUserById(id);
+    }
+}
