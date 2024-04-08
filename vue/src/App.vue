660fdbb15b6633b5b06ad72ab3ac54befb38c1f6
Roberto Cedeno <150482419+robced@users.noreply.github.com>
2024-04-08 09:08:42 -0700

    feat: complete navbar w/o hrefs

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div id="capstone-app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
+  <div id="capstone-app" class="">
+      <Navbar/>
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>
