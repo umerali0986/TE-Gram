ad2ceef911f1cca99ac4b017ff4e0f4c889fb276
Dustin Dobos <dustindobos753@gmail.com>
2024-04-16 14:51:26 -0400

    Added updateUser to front-end

--- java/src/main/java/com/techelevator/dao/JdbcUserDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcUserDao.java
@@ -110,18 +110,18 @@ public class JdbcUserDao implements UserDao {
 
     @Override
     public User updateUser(User user, int currentUserId) {
-        if(user.getId() != currentUserId){
-            throw new DaoException("Not valid user");
-        }
+//        if(user.getId() != currentUserId){
+//            throw new DaoException("Not valid user");
+//        }
         User updatedUser = null;
 
-        String sql = "UPDATE users SET email = ?, name = ? " +
+        String sql = "UPDATE users SET email = ?, name = ?, avatar = ? " +
                 "WHERE user_id = ?";
 
         try{
             int numberOfRows = 0;
             numberOfRows = jdbcTemplate.update(sql, user.getEmail()
-                    , user.getName(), user.getId());
+                    , user.getName(), user.getAvatar(), currentUserId);
 
             if(numberOfRows == 0){
                 throw new DaoException("No number of rows affected");
