6aafe905ef0f4ce8eed1b8b5d5396ca7f72c14dc
Roberto Cedeno <e@robced.com>
2024-04-18 19:54:14 +0000

    add info for team

--- vue/src/components/Sidebar.vue
+++ vue/src/components/Sidebar.vue
@@ -43,7 +43,7 @@
                   stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
 
-              Saved
+              Favorite
             </button>
           </router-link>
 
